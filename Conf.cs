c47ee9c767cf1137ab958656d04be67e595eba81
hengheng <1175615214@qq.com>
2022-07-18 16:36:13 +0800

    增加conf.json文件

--- Conf.cs
+++ Conf.cs
@@ -1,5 +1,10 @@
 public class Conf
 {
+    /// <summary>
+    /// 配置文件的类型，LOCAL_FILE: 表示conf文件夹下的conf.json文件；ACTION_SECRET:表示配置在github-action中的secret
+    /// </summary>
+    /// <value></value>
+    public string ConfType { get; set; }
     public User[] Users { get; set; }
     public string ScKey { get; set; }
     public string ScType { get; set; }
